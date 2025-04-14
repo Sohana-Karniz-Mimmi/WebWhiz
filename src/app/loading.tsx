9af185139374895a8d7b897715a9714b1c0c4f94
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2025-04-14 18:55:45 +0600

    added block section

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 const loading = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div class="loader"></div>
+      <div className="loader"></div>
     </div>
   );
 };
