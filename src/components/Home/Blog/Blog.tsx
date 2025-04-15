3c2310f987bb5c1a73c38afbb8965f0e3f54843c
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2025-04-15 10:01:31 +0600

    fixed some issues

--- src/components/Home/Blog/Blog.tsx
+++ src/components/Home/Blog/Blog.tsx
@@ -19,7 +19,7 @@ const Blog = () => {
   };
 
   return (
-    <div className="custom_container xl:pt-[212px] md:pt-[42px] pt-[60px] pb-[42px] relative">
+    <div className="custom_container xl:pt-[212px] md:pt-[155px] pt-[60px] pb-[42px] relative">
       {/* Mobile: Title First */}
       <div className="md:hidden flex flex-col">
         <div className="text-center mb-[8px]">
